a49499887031abc1ed323990bcdfdc25bbc2edd4
Brad <bjcost@gmail.com>
2023-02-11 22:15:03 -0500

    completed challenge 3

--- Challenge03/site03.js
+++ Challenge03/site03.js
@@ -2,22 +2,40 @@
 function convertIt() {
 
     //get the user entered value
-    let kmValue = parseFloat(document.getElementById("InputValue").value);
+    let kmValue = parseFloat(document.getElementById("kmInputValue").value);
+    let faValue = parseFloat(document.getElementById("faInputValue").value);
 
-    if (isNaN(kmValue) ) {
-        alert("Please enter a number");
-
-    } else {
-        //call your function to conver the vm value to miles
+    if (!isNaN(kmValue) ) {
+        //call your function to convert the vm value to miles
         let milesValue = convertToKm(kmValue);
 
-        //display your name 
+        //display your value 
         document.getElementById("results").innerHTML = `${kmValue} km = ${milesValue.toFixed(2)} miles`;
+    }
+
+    if (!isNaN(faValue) ) {
+        //call your function to convert the vm value to miles
+        let celsiusValue = convertToFa(faValue);
+
+        //display your value 
+        document.getElementById("resultsFa").innerHTML = `${faValue} fahrenheit = ${celsiusValue.toFixed(2)} celsius`;
     }
 
 }
 
 //takes an array of strings and returns the longest one. 
 function convertToKm(kmValue) {
-   return 0;
+    let milesValue = 0;
+
+    milesValue = (kmValue * 0.6214);
+
+    return milesValue;
+}
+
+function convertToFa(faValue) {
+    let celsiusValue = 0;
+
+    celsiusValue = (faValue - 32) * 0.5556;
+
+    return celsiusValue;
 }
